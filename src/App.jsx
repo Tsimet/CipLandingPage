7e88b44a00eee494b421e0c56bece3bde7dfbbdf
savaDjordjevic <djordjevicasava@gmail.com>
2025-12-09 16:30:35 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import HelloWorld from "./components/HelloWorld/HelloWorld";
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HelloWorld/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import styles from "./HelloWorld.module.css";
+
+export default function HelloWorld() {
+  return (
+    <>
+      <div className={styles.klass}>Hello World!</div>
+    </>
+  );
+}
