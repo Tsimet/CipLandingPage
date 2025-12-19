3b2a1ed8a35d2dacc001d0a8091bd43fff586d67
savaDjordjevic <djordjevicasava@gmail.com>
2025-12-19 15:10:47 +0100

    Add the Map component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Clients from "./components/Clients/Clients";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
+import Map from "./components/Map/Map";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <WhyChooseUs />
       <AboutUs />
       <Clients />
+      <Map />
+      <Footer />
     </>
   );
 }
