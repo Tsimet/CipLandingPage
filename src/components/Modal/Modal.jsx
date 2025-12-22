8ec0ab17d10faf32dae1c65f7cae9b89c6b8abca
savaDjordjevic <djordjevicasava@gmail.com>
2025-12-22 03:14:39 +0100

    Handle esc key for exiting Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 
 export default function Modal({ children, onClose }) {
+  useEffect(() => {
+    const handleEsc = (e) => {
+      if (e.key === "Escape") onClose();
+    };
+    window.addEventListener("keydown", handleEsc);
+
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, [onClose]);
+
   return ReactDOM.createPortal(
     <div className={styles.modal__backdrop} onClick={onClose}>
       <div
