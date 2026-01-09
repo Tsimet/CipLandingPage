d0b8f2448e2d0e1835f7afbe8b2b4069572e6c01
savaDjordjevic <djordjevicasava@gmail.com>
2026-01-09 22:04:18 +0100

    Add responsive logo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import styles from "./Hero.module.css";
 import logo from "../../assets/images/logo.png";
+import logo__small from "../../assets/images/logo-old.png";
 import heroVideo from "../../assets/video/hero__video.mp4";
 
 export default function Hero() {
@@ -13,7 +14,16 @@ export default function Hero() {
 
       <div className={styles.hero__content}>
         <h1>relocation and installation</h1>
-        <img src={logo} alt="CIP Logo" className={styles.hero__logo}></img>
+        {/* <img src={logo} alt="CIP Logo" className={styles.hero__logo}></img> */}
+        <picture className={styles.hero__logo}>
+          <source media="(max-width: 780px)" srcSet={logo__small} />
+          <img
+            src={logo}
+            alt="Company Logo"
+            style={{ width: "auto" }}
+            className={styles.hero__logo}
+          />
+        </picture>
         <h3>Realiable Partner In Industry</h3>
         <a href="#about" className={styles.btn__scroll}>
           More About Us
